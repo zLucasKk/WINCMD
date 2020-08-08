49f52e15d77fb965c261b6d61f33ba6fa3b6ec0d
Lucas Gabriel <44913206+zLucasKk@users.noreply.github.com>
2020-08-08 18:56:34 -0300

    Rainbow Windows Terminal
    Coloreful Windows Terminal 🌈
    
    ⚠WARNING, THIS MAY CAUSE LAGS ON YOUR DEVICE OR EPILEPSY, PLEASE USE WITH CAUTION.
    
    That's a joke, anything here is for a real purpose, you're free to use it on yours projects. :)

---
+++ RainbowWindowsTerminal.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+
+::RainbowTerminal (ONLY TEXT)
+::That's a joke, anything here is for a real purpose, you're free to use it on yours projects. :)
+::WARNING, THIS MAY CAUSE LAGS ON YOUR DEVICE OR EPILEPSY, PLEASE USE WITH CAUTION.
+
+::Author: zLucasKkj
+::Version: RELEASE 1.0.0
+
+
+::delete this string stack if you does't want to show this warning.
+
+color 0c
+echo WARNING, THIS MAY CAUSE LAGS ON YOUR DEVICE OR EPILEPSY, PLEASE USE WITH CAUTION.
+echo PRESS ANY KEY TO IGNORE THIS WARNING.
+pause >nul
+cls
+
+::------------------------------CLASSES-----------------------------------::
+
+echo "Put your text for first showing or anything else you want."
+:rainbowterminal.main
+	echo "Put your text in here FOR REPEAT or anything else you want" 
+	color 0c
+	color 06 
+	color 0e
+	color 0a
+	color 0b
+	color 01
+	color 0d
+goto rainbowterminal.main
+
+
+::END, THANKS FOR USE.
